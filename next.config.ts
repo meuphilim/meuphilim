8fa494a1b6365cd6d5d78f88defc71a7b35e4019
meuphilim <meuphilim@gmail.com>
2025-06-05 00:26:32 -0400

    Atualizando dependências do Next.js para a versão 14.0.0

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export', // Essencial para GitHub Pages
   // Se o seu site GitHub Pages for servido de um subdiretório (ex: https://<username>.github.io/<repository-name>/),
   // você precisará descomentar e configurar basePath e assetPrefix.
   // Exemplo para um repositório chamado 'meu-app':
-  // basePath: '/meu-app',
-  // assetPrefix: '/meu-app/',
+  basePath: '/meuphilim',
+  assetPrefix: '/meuphilim/',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -30,4 +28,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
